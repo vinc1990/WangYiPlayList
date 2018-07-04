5aca1cdb1e98b18b8add2edab1bca215eb33e4f9
vinc1990 <vinc2012@163.com>
2018-07-04 17:14:07 +0800

    Add files via upload

---
+++ WangYiPlayList.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import csv
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+
+url = 'http://music.163.com/#/discover/playlist/?order=hot&cat=%E5%85%A8%E9%83%A8&limit=35&offset=0'
+
+
+
+
+csv_file = open('playlist.csv','w',newline='',errors='ignore')
+writer = csv.writer(csv_file)
+writer.writerow(['标题','播放数','链接'])
+
+while url != 'javascript:void(0)':
+
+    driver.get(url)
+
+    driver.switch_to.frame("contentFrame")
+
+    data = driver.find_element_by_id("m-pl-container").\
+        find_elements_by_tag_name("li")
+
+
+    for i in range(len(data)):
+
+        nb = data[i].find_element_by_class_name("nb").text
+        if '万' in nb and int(nb.split("万")[0]) > 500:
+     
+            msk = data[i].find_element_by_css_selector("a.msk")
+            print([msk.get_attribute("title"),nb,msk.get_attribute("href")])
+            writer.writerow([msk.get_attribute("title").encode('UTF-8').decode('UTF-8'),nb,msk.get_attribute("href")])
+
+
+    url = driver.find_element_by_css_selector("a.zbtn.znxt").\
+    get_attribute('href')
+
+csv_file.close();
